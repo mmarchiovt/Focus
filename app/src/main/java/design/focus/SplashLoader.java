86f406bfc6f04ba544b01528c00e5f48e5f23cd7
johncmyhre <johncmyhre@gmail.com>
2015-11-12 16:57:25 -0500

    Updated splash screen icon and made it fullscreen

--- app/src/main/java/design/focus/SplashLoader.java
+++ app/src/main/java/design/focus/SplashLoader.java
@@ -2,12 +2,19 @@ package design.focus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashLoader extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_loader);
     }
 
