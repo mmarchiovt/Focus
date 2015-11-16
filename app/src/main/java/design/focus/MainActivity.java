b8f799f8706c27b1e6dfc4178fb0b1c5e7dccb2c
Marchio <mmarchio@vt.edu>
2015-11-16 16:43:06 -0500

    Using bitmap instead of png. improved performance

--- app/src/main/java/design/focus/MainActivity.java
+++ app/src/main/java/design/focus/MainActivity.java
@@ -1,5 +1,8 @@
 package design.focus;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -15,6 +18,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener
@@ -51,6 +55,14 @@ public class MainActivity extends AppCompatActivity
         watch = (ImageButton) findViewById(R.id.watch);
         speech = (ImageButton) findViewById(R.id.speech);
 
+        lightbulb.setImageBitmap(
+                decodeSampledBitmapFromResource(getResources(), R.drawable.lightbulb_onetemp, 100, 100));
+        heart.setImageBitmap(
+                decodeSampledBitmapFromResource(getResources(), R.drawable.heart_onetemp, 100, 100));
+        watch.setImageBitmap(
+                decodeSampledBitmapFromResource(getResources(), R.drawable.watch_onetemp, 100, 100));
+        speech.setImageBitmap(
+                decodeSampledBitmapFromResource(getResources(), R.drawable.speak_onetemp, 100, 100));
 
         lightbulb.setOnClickListener(this);
         heart.setOnClickListener(this);
@@ -178,4 +190,46 @@ public class MainActivity extends AppCompatActivity
             }
         }
     }
+
+
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
 }
--- app/src/main/java/design/focus/SplashLoader.java
+++ app/src/main/java/design/focus/SplashLoader.java
@@ -21,7 +21,6 @@ public class SplashLoader extends AppCompatActivity implements View.OnClickListe
     private Button loadingBar;
     private ImageView icon;
 
-
     protected void onCreate(Bundle savedInstanceState) {
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -29,6 +28,10 @@ public class SplashLoader extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_splash_loader);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        icon = (ImageView) findViewById(R.id.icon);
+        icon.setImageBitmap(
+                decodeSampledBitmapFromResource(getResources(), R.drawable.icon, 100, 100));
 
         loadingBar = (Button) findViewById(R.id.progressBar);
         loadingBar.setOnClickListener(this);
@@ -46,6 +49,45 @@ public class SplashLoader extends AppCompatActivity implements View.OnClickListe
     {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
     }
 
 }
