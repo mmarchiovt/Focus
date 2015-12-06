7c887ab80528b796d768eb40758d549749c738ae
Marchio <mmarchio@vt.edu>
2015-12-06 17:37:50 -0500

    clean splash

--- app/src/main/java/design/focus/SplashLoader.java
+++ app/src/main/java/design/focus/SplashLoader.java
@@ -43,7 +43,7 @@ public class SplashLoader extends AppCompatActivity {
             setContentView(R.layout.activity_splash_loader);
             icon = (ImageView) findViewById(R.id.icon);
             icon.setImageBitmap(
-                    decodeSampledBitmapFromResource(getResources(), R.drawable.icon, 100, 100));
+                    BitmapHelper.decodeSampledBitmapFromResource(getResources(), R.drawable.icon, 100, 100));
             layout = (LinearLayout) findViewById(R.id.splash);
         }
 
@@ -96,48 +96,6 @@ public class SplashLoader extends AppCompatActivity {
         first = true;
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-    }
-
-
-//    Helpers
-
-    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
-                                                         int reqWidth, int reqHeight) {
-
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeResource(res, resId, options);
-
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeResource(res, resId, options);
-    }
-
-    public static int calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
-        if (height > reqHeight || width > reqWidth) {
-
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
-
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) > reqHeight
-                    && (halfWidth / inSampleSize) > reqWidth) {
-                inSampleSize *= 2;
-            }
-        }
-
-        return inSampleSize;
     }
 
 }
